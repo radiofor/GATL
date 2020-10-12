e9635697f484cc1ed5e45984a277afdb94efa488
radiofor <572722344@qq.com>
2020-10-12 21:17:52 +0800

    update

--- geotiff.py
+++ geotiff.py
@@ -15,31 +15,41 @@ suffix = ['.tif', '.tiff']
 
 class Extraction:
     @staticmethod
-    def extent_polygon(src_path):
-        with gdal.Open(src_path['path']) as src_ds:
-            ncol = src_ds.RasterXSize
-            nrow = src_ds.RasterYSize
-            geotrsfrm = src_ds.GetGeoTransform()
+    def extent_polygon(ncol, nrow, geotrsfrm):
+        ring = ogr.Geometry(ogr.wkbLinearRing)
+        ring.AddPoint(geotrsfrm[0], geotrsfrm[3])
+        ring.AddPoint(geotrsfrm[0] + ncol * geotrsfrm[1], geotrsfrm[3])
+        ring.AddPoint(geotrsfrm[0] + ncol * geotrsfrm[1], geotrsfrm[3] + nrow * geotrsfrm[5])
+        ring.AddPoint(geotrsfrm[0], geotrsfrm[3] + nrow * geotrsfrm[5])
+        ring.AddPoint(geotrsfrm[0], geotrsfrm[3])
 
-            ring = ogr.Geometry(ogr.wkbLinearRing)
-            ring.AddPoint(geotrsfrm[0], geotrsfrm[3])
-            ring.AddPoint(geotrsfrm[0] + ncol * geotrsfrm[1], geotrsfrm[3])
-            ring.AddPoint(geotrsfrm[0] + ncol * geotrsfrm[1], geotrsfrm[3] + nrow * geotrsfrm[5])
-            ring.AddPoint(geotrsfrm[0], geotrsfrm[3] + nrow * geotrsfrm[5])
-            ring.AddPoint(geotrsfrm[0], geotrsfrm[3])
+        polygon = ogr.Geometry(ogr.wkbPolygon)
+        polygon.AddGeometry(ring)
 
-            polygon = ogr.Geometry(ogr.wkbPolygon)
-            polygon.AddGeometry(ring)
-
-            return polygon
-
+        return polygon
 
     @staticmethod
     def export_extent(src_path, dst_path):
         file_paths = fileman.get_all_files(src_path, suffix)
         for file_path in file_paths['file']:
-            polygon =
+            src_ds = gdal.Open(file_path['path'])
+            if src_ds is None:
+                continue
+            ncol = src_ds.RasterXSize
+            nrow = src_ds.RasterYSize
+            geotrsfrm = src_ds.GetGeoTransform()
+            srs = osr.SpatialReference(wkt=src_ds.GetProjection())
+
+            dst_name = file_path['name'] + '.shp'
+            driver = ogr.GetDriverByName("ESRI Shapefile")
+            dst_ds = driver.CreateDataSource(os.path.join(dst_path, dst_name))
+            dst_layer = dst_ds.CreateLayer('extent', srs=srs)
+            dst_feat = ogr.Feature(dst_layer.GetLayerDefn())
+
+            polygon = Extraction.extent_polygon(ncol, nrow, geotrsfrm)
+            dst_feat.SetGeometry(polygon)
+            dst_layer.CreateFeature(dst_feat)
 
 
 if __name__ == '__main__':
-    Extraction.layer_extent(r'I:\QZGY2m-1', r'I:')
+    Extraction.export_extent(r'J:\QZGY2m-1', r'J:\Extent')
--- shapefile.py
+++ shapefile.py
@@ -13,14 +13,16 @@ suffix = ['.py']
 class Geometry:
     @staticmethod
     def disolve(src_path):
-        file_paths = fileman.get_all_files(r'F:\Projects\GATL', suffix)
+        file_paths = fileman.get_all_files(src_path, suffix)
         for file_path in file_paths['file']:
             driver = ogr.GetDriverByName("ESRI Shapefile")
             src_ds = driver.Open(src_path['path'], 0)
 
     @staticmethod
-    def
+    def merge(src_path):
+        file_paths = fileman.get_all_files(src_path, suffix)
 
 
 if __name__ == '__main__':
-    Geometry.disolve()
+    # Geometry.disolve()
+    Geometry.merge(r'J:\Extent')
